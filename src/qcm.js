8bbea252a26dff250d13bc4853910cc171f8beb9
GuillaumeMaurin <guillaume.maurin@edu.ece.fr>
2018-11-06 16:18:19 +0900

    pie fix

--- src/qcm.js
+++ src/qcm.js
@@ -70,5 +70,32 @@ function calcul() {
       document.getElementById("txtresult").innerHTML = "You have " + point + " good answers and " + (nbrQ - point) + " wrong answers";
       document.getElementById('cardresult').style.visibility = 'visible';
 
+      //pie print
+      var options = {
+         title: {
+            text: ""
+         },
+         /*title: {
+         	text: "QCM Result"
+         },*/
+         data: [{
+            type: "pie",
+            startAngle: 45,
+            showInLegend: "false",
+            legendText: "{label}",
+            indexLabel: "{label} : {y}",
+            yValueFormatString: "#,##0.#" % "",
+            dataPoints: [{
+                  label: "Correct",
+                  y: point
+               }, // (point*100/nbrQ) },
+               {
+                  label: "Wrong",
+                  y: (nbrQ - point)
+               } //((nbrQ - point)*100/nbrQ) }
+            ]
+         }]
+      };
+      $("#chartContainer").CanvasJSChart(options);
    }
 }
